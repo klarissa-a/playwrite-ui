5efa86d4b0f76fe254c1a788481338020c313069
klarissa-a <kudryakova.larisa@gmail.com>
2024-08-10 18:13:17 +0300

    изменить репортер

--- playwright.config.js
+++ playwright.config.js
@@ -20,9 +20,9 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: 2, // process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 1, // process.env.CI ? 1 : undefined,  
+  workers: process.env.CI ? 1 : undefined,  
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'line' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
